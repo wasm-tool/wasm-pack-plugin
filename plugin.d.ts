9c378a647aa4a328b13ccebcaab6898d7ffcab96
clearloop <udtrokia@gmail.com>
2020-01-22 02:07:25 +0800

    ts: declare outDir and outName in options

--- plugin.d.ts
+++ plugin.d.ts
@@ -4,9 +4,11 @@ declare module '@wasm-tool/wasm-pack-plugin' {
     export interface WasmPackPluginOptions {
         crateDirectory: string;
         extraArgs?: string;
-        watchDirectories?: string[];
         forceWatch?: boolean;
         forceMode?: 'development' | 'production';
+        outDir?: string;
+        outName?: string;
+        watchDirectories?: string[];
     }
 
     export default class WasmPackPlugin extends Plugin {
