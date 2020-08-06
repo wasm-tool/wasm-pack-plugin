ca2e7facefa51c5848507168ffe1ee4cf6947199
Cole Lawrence <cole@colelawrence.com>
2020-08-06 09:37:39 -0400

    Fix plugin.d.ts exported type declarations

--- plugin.d.ts
+++ plugin.d.ts
@@ -1,20 +1,26 @@
 import { Plugin } from 'webpack';
 
-declare module '@wasm-tool/wasm-pack-plugin' {
-    export interface WasmPackPluginOptions {
-        crateDirectory: string;
-        args?: string;
-        extraArgs?: string;
-        forceWatch?: boolean;
-        forceMode?: 'development' | 'production';
-        outDir?: string;
-        outName?: string;
-        watchDirectories?: string[];
-        /** Controls plugin output verbosity. Defaults to 'info'. */
-        pluginLogLevel?: 'info' | 'error';
-    }
+export interface WasmPackPluginOptions {
+    crateDirectory: string;
+    args?: string;
+    extraArgs?: string;
+    forceWatch?: boolean;
+    forceMode?: 'development' | 'production';
+    outDir?: string;
+    outName?: string;
+    watchDirectories?: string[];
+    /** Controls plugin output verbosity. Defaults to 'info'. */
+    pluginLogLevel?: 'info' | 'error';
+}
 
-    export default class WasmPackPlugin extends Plugin {
-        constructor(options: WasmPackPluginOptions)
-    }
+export default class WasmPackPlugin extends Plugin {
+    constructor(options: WasmPackPluginOptions)
+}
+
+export = WasmPackPlugin
+
+declare module '@wasm-tool/wasm-pack-plugin' {
+    export { WasmPackPluginOptions, WasmPackPlugin }
+    export default WasmPackPlugin
+    export = WasmPackPlugin
 }
