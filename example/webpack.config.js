22a242e16628f4aa5c82eba0c13203962b21cb55
Sven Sauleau <github@sauleau.com>
2021-07-24 22:44:48 +0100

    Update webpack.config.js

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
